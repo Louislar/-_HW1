c39712ee92986f94545abd3a84c25e8db73907fe
Louislar <a8945267@gmail.com>
2018-03-28 19:33:04 +0800

    2018/3/28 19:36

--- HW2.asm
+++ HW2.asm
@@ -3,6 +3,7 @@
 	change_line: .asciiz "\n"
      text:  .asciiz "\nInput: \n"
   turn: .word 0
+  k: .word 0
 
 
 .text
@@ -19,20 +20,72 @@ read:
     li $v0, 4
     la $a0, text
     syscall
+    #read in integer, k, which is the number of a_i's i also m_i's i
+    li      $v0, 5
+    syscall
+    # store k in $s0 (always)
+    move $s0, $v0
+    
+    # We will input k*2 data
+    addu $t0, $s0, $s0
+input:
     #read in integer
     li      $v0, 5
     syscall
+    #push the input integer in stack
+    addi $sp, $sp, -4
+    sw $v0, 0($sp)
     
     
-    li $a0, 7
-    li $a1, 5
-    jal gcd
+    subi $t0, 1  # loop 2*k times
+    bne $t0, 0, input
+	
+	#we input k a_i first, then input k m_i
+	#, but we need m_i first
+	# store the $sp at $s1
+	move $s1, $sp
+	addu $t0, $s0,$s0 # loop k*2 times 
+s1_adjust:	# adjust $s1 to storing m_i's data's address (always)
+	addi $s1, 4
+	subi $t0, 1
+	bne $t0, 0, s1_adjust
+	
+	
+	#we need to multiply k m_i, to get m, and store it in $s2
+	move $t0, $s0 # loop k times
+	move $t1, $s1 # store m_i's data's address (temp)
+	li $s2, 0 # store m at $s2 (always)
+cal_m:
+	lw $t2, 0($t1) # load m_i to $t2
+	mul $s2, $s2, $t2 # mlutiply s2 by t2, then store the result in s2
+	addi $t1, -4
+	subi $t0, 1
+	bne $t0, 0, cal_m
+	
+	
+
+	#we ned to count k Y_i, so loop k times
+	move $t0, $s0
+	move $t1, $s1 # store m_i's data's address (temp)
+count_Y_i: # $t0 cannot use
+	addi $t1, -4 # make $t1 point to m_i
+	lw $t2, 0($t1) # load m_i to $t2 (temp)
+	
+	#calulate M_i
+	div $s2, $t2 # m/m_i
+	mflo $t3 # $t3 stores M_i
+	
+    li $a0, $t3 # $a0 is M_i
+    li $a1, 5 # $a1 is m_i
+    jal gcd # i will store in $v0, and j will store in $v1
+    		# 1 = i*M_i + j*m_i
     
-    #print i, j
-    move $s0, $v0 #$s0=i
-    move $s1, $v1 #$s1=j
+    
+    #print i
+    move $s1, $v0 #$s1=i
+    move $s2, $v1 #$s2=j
     li $v0, 1
-    move $a0, $s0
+    move $a0, $s1
     syscall
     
     # Printing out the \n
@@ -40,9 +93,16 @@ read:
     la $a0, change_line
     syscall
     
+    #print j
     li $v0, 1
-    move $a0, $s1
+    move $a0, $s2
     syscall
+    
+    #push
+    
+    subi $t0, 1
+    bne $t0, 0, count_Y_i
+    
     
     j end
     
